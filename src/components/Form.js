625570b3389f2591c0f5d2b59abddc219513f4c4
Thomas Bakken <tbakken14@gmail.com>
2023-09-08 20:40:16 -0700

    Update form inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,8 +4,19 @@ function Form(props){
     return ( 
     <React.Fragment>
         <div className="Form">
-            <form onSubmit={(e) => props.onSubmit(e)}>
-                <input type="text"/>
+            <form onSubmit={props.onSubmit}>
+                <label for="name">Name:</label>
+                <input id="name" type="text" name="name" required/>
+                <br/>
+                <label for="origin">Origin:</label>
+                <input id="origin" type="text" name="origin" required/>
+                <br/>
+                <label for="price">Price:</label>
+                <input id="price" type="number" name="price" required/>
+                <br/>
+                <label for="roast">Roast:</label>
+                <input id="roast" type="text" name="roast" required/>
+                <br/>
                 <input type="submit" value="Submit" />
             </form>
         </div>
