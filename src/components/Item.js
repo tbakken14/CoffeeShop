2d6df25cfb8edd248bb32e09d91859d3dd68c3e4
Thomas Bakken <tbakken14@gmail.com>
2023-09-14 01:55:24 -0700

    Add decrement quantity functionality to item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,31 @@
 import React from "react";
 
-function Item(props){
-    return ( 
-    <React.Fragment>
-        <div className="item">{props.name}</div>
-    </React.Fragment>
-    );
+class Item extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quantity : 100
+        }
+    }
+
+    decrementQuantity() { 
+        this.setState({ quantity : this.state.quantity - 10 });
+    }
+
+    render() {
+        return ( 
+            <React.Fragment>
+                <div className="item">
+                    <h1>{this.props.name}</h1>
+                    <h1>{this.props.origin}</h1>
+                    <h1>{this.props.price}</h1>
+                    <h1>{this.props.roast}</h1>
+                    <h1>{this.state.quantity}</h1>
+                    <button onClick={this.decrementQuantity.bind(this)}>Sell Beans</button>
+                </div>
+            </React.Fragment>
+            );
+    }
 }
 
 export default Item;
