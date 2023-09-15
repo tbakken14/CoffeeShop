756eb6e934bdecb1eae2afc995cb915afc140307
Thomas Bakken <tbakken14@gmail.com>
2023-09-15 00:34:13 -0700

    Fix x scroll style

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,13 @@
-import React from "react";
 import Item from "./Item";
 
 function Menu(props){
   return ( 
-    <React.Fragment>
-        <h1>Menu</h1>
-        <div className="Menu">
-            {props.items.map((e, i) => <Item {...e} key={i}/>)}
-        </div>
-    </React.Fragment>
+    <>
+      <h1>Menu</h1>
+      <div className="Menu">
+        {props.items.map((e, i) => <Item {...e} key={i}/>)}
+      </div>
+    </>
   );
 }
 
